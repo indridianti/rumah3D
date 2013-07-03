4a8e1b480f1cc867cf5656f6e2cec8c4662c9bb4
Indri Dianti Hapsari <diantihapsari@gmail.com>
2013-07-03 16:09:20 +0700

    Update rumah3D.cpp

--- rumah3D.cpp
+++ rumah3D.cpp
@@ -1,6 +1,285 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
-static float ypoz = 0, zpoz = 0, xpoz = 0,a = 0, b = 0,c = -10;
+#include <GL/glu.h>
+#include <GL/gl.h>
+#include "imageloader.h"
+#include "vec3f.h"
+#endif
+
+static GLfloat spin, spin2 = 0.0;
+float angle = 0;
+using namespace std;
+
+float lastx, lasty;
+GLint stencilBits;
+static int viewx = 50;
+static int viewy = 24;
+static int viewz = 80;
+int a=0, b=0, c=0, d=0;
+float rot = 0;
+
+//train 2D
+//class untuk terain 2D
+class Terrain {
+private:
+  int w; //Width
+	int l; //Length
+	float** hs; //Heights
+	Vec3f** normals;
+	bool computedNormals; //Whether normals is up-to-date
+public:
+	Terrain(int w2, int l2) {
+		w = w2;
+		l = l2;
+
+		hs = new float*[l];
+		for (int i = 0; i < l; i++) {
+			hs[i] = new float[w];
+		}
+
+		normals = new Vec3f*[l];
+		for (int i = 0; i < l; i++) {
+			normals[i] = new Vec3f[w];
+		}
+
+		computedNormals = false;
+	}
+
+	~Terrain() {
+		for (int i = 0; i < l; i++) {
+			delete[] hs[i];
+		}
+		delete[] hs;
+
+		for (int i = 0; i < l; i++) {
+			delete[] normals[i];
+		}
+		delete[] normals;
+	}
+
+	int width() {
+		return w;
+	}
+
+	int length() {
+		return l;
+	}
+
+	//Sets the height at (x, z) to y
+	void setHeight(int x, int z, float y) {
+		hs[z][x] = y;
+		computedNormals = false;
+	}
+
+	//Returns the height at (x, z)
+	float getHeight(int x, int z) {
+		return hs[z][x];
+	}
+
+	//Computes the normals, if they haven't been computed yet
+	void computeNormals() {
+		if (computedNormals) {
+			return;
+		}
+
+		//Compute the rough version of the normals
+		Vec3f** normals2 = new Vec3f*[l];
+		for (int i = 0; i < l; i++) {
+			normals2[i] = new Vec3f[w];
+		}
+
+		for (int z = 0; z < l; z++) {
+			for (int x = 0; x < w; x++) {
+				Vec3f sum(0.0f, 0.0f, 0.0f);
+
+				Vec3f out;
+				if (z > 0) {
+					out = Vec3f(0.0f, hs[z - 1][x] - hs[z][x], -1.0f);
+				}
+				Vec3f in;
+				if (z < l - 1) {
+					in = Vec3f(0.0f, hs[z + 1][x] - hs[z][x], 1.0f);
+				}
+				Vec3f left;
+				if (x > 0) {
+					left = Vec3f(-1.0f, hs[z][x - 1] - hs[z][x], 0.0f);
+				}
+				Vec3f right;
+				if (x < w - 1) {
+					right = Vec3f(1.0f, hs[z][x + 1] - hs[z][x], 0.0f);
+				}
+
+				if (x > 0 && z > 0) {
+					sum += out.cross(left).normalize();
+				}
+				if (x > 0 && z < l - 1) {
+					sum += left.cross(in).normalize();
+				}
+				if (x < w - 1 && z < l - 1) {
+					sum += in.cross(right).normalize();
+				}
+				if (x < w - 1 && z > 0) {
+					sum += right.cross(out).normalize();
+				}
+
+				normals2[z][x] = sum;
+			}
+		}
+
+		//Smooth out the normals
+		const float FALLOUT_RATIO = 0.5f;
+		for (int z = 0; z < l; z++) {
+			for (int x = 0; x < w; x++) {
+				Vec3f sum = normals2[z][x];
+
+				if (x > 0) {
+					sum += normals2[z][x - 1] * FALLOUT_RATIO;
+				}
+				if (x < w - 1) {
+					sum += normals2[z][x + 1] * FALLOUT_RATIO;
+				}
+				if (z > 0) {
+					sum += normals2[z - 1][x] * FALLOUT_RATIO;
+				}
+				if (z < l - 1) {
+					sum += normals2[z + 1][x] * FALLOUT_RATIO;
+				}
+
+				if (sum.magnitude() == 0) {
+					sum = Vec3f(0.0f, 1.0f, 0.0f);
+				}
+				normals[z][x] = sum;
+			}
+		}
+
+		for (int i = 0; i < l; i++) {
+			delete[] normals2[i];
+		}
+		delete[] normals2;
+
+		computedNormals = true;
+	}
+
+	//Returns the normal at (x, z)
+	Vec3f getNormal(int x, int z) {
+		if (!computedNormals) {
+			computeNormals();
+		}
+		return normals[z][x];
+	}
+};
+//end class
+
+
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glShadeModel(GL_SMOOTH);
+}
+
+//Loads a terrain from a heightmap.  The heights of the terrain range from
+//-height / 2 to height / 2.
+//load terain di procedure inisialisasi
+Terrain* loadTerrain(const char* filename, float height) {
+	Image* image = loadBMP(filename);
+	Terrain* t = new Terrain(image->width, image->height);
+	for (int y = 0; y < image->height; y++) {
+		for (int x = 0; x < image->width; x++) {
+			unsigned char color = (unsigned char) image->pixels[3 * (y
+					* image->width + x)];
+			float h = height * ((color / 255.0f) - 0.5f);
+			t->setHeight(x, y, h);
+		}
+	}
+
+	delete image;
+	t->computeNormals();
+	return t;
+}
+
+float _angle = 60.0f;
+//buat tipe data terain
+Terrain* _terrain;
+Terrain* _terrainTanah;
+Terrain* _terrainAir;
+
+const GLfloat light_ambient[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+const GLfloat light_diffuse[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+const GLfloat light_ambient2[] = { 0.3f, 0.3f, 0.3f, 0.0f };
+const GLfloat light_diffuse2[] = { 0.3f, 0.3f, 0.3f, 0.0f };
+
+const GLfloat mat_ambient[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+void cleanup() {
+	delete _terrain;
+	delete _terrainTanah;
+}
+
+//untuk di display
+void drawSceneTanah(Terrain *terrain, GLfloat r, GLfloat g, GLfloat b) {
+	//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	/*
+	 glMatrixMode(GL_MODELVIEW);
+	 glLoadIdentity();
+	 glTranslatef(0.0f, 0.0f, -10.0f);
+	 glRotatef(30.0f, 1.0f, 0.0f, 0.0f);
+	 glRotatef(-_angle, 0.0f, 1.0f, 0.0f);
+
+	 GLfloat ambientColor[] = {0.4f, 0.4f, 0.4f, 1.0f};
+	 glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+
+	 GLfloat lightColor0[] = {0.6f, 0.6f, 0.6f, 1.0f};
+	 GLfloat lightPos0[] = {-0.5f, 0.8f, 0.1f, 0.0f};
+	 glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+	 glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+	 */
+	float scale = 500.0f / max(terrain->width() - 1, terrain->length() - 1);
+	glScalef(scale, scale, scale);
+	glTranslatef(-(float) (terrain->width() - 1) / 2, 0.0f,
+			-(float) (terrain->length() - 1) / 2);
+
+	glColor3f(r, g, b);
+	for (int z = 0; z < terrain->length() - 1; z++) {
+		//Makes OpenGL draw a triangle at every three consecutive vertices
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int x = 0; x < terrain->width(); x++) {
+			Vec3f normal = terrain->getNormal(x, z);
+			glNormal3f(normal[0], normal[1], normal[2]);
+			glVertex3f(x, terrain->getHeight(x, z), z);
+			normal = terrain->getNormal(x, z + 1);
+			glNormal3f(normal[0], normal[1], normal[2]);
+			glVertex3f(x, terrain->getHeight(x, z + 1), z + 1);
+		}
+		glEnd();
+	}
+
+}
+
+void jalan()
+{
+    glPushMatrix();
+        glColor3f(1.0f, 1.0f, 1.0f);
+    glScaled(10.5, 0.1, 2.5);
+    glutSolidCube(0.5f);
+    glPopMatrix();
+}
+
 void coba(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,int z1,float z2,float z3){
 glColor3f(1.0, 0.3, 0.2); //sisi depan
 glVertex3d(x1,y1,z2);
@@ -56,157 +335,252 @@ glVertex3d(x4,y4,z2);
 glVertex3d(x4,y1,z3);
 glVertex3d(x3,y1,z3);
 }
-void init(void)
-{
-glClearColor (0.0, 1.0, 1.0, 0.0);
-glOrtho(-12,12,-6,6,-35,35);
-glEnable(GL_DEPTH_TEST);
-glShadeModel (GL_SMOOTH);
-glMatrixMode (GL_PROJECTION);
-glLoadIdentity ();
-gluPerspective(80.0,2.2, 1.0, 20.0);
-glMatrixMode (GL_MODELVIEW);
-}
-void display(void)
-{
-glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-glLoadIdentity (); //digunakan untuk me-nonaktifkan/me-reset
 
-glTranslatef(a,b,c); //digunakan untuk melakukan perpindahan posisi gelombang
-glRotatef(xpoz,1,0,0);
-glRotatef(ypoz,0,1,0);
-glRotatef(zpoz,0,0,1);
-glBegin(GL_QUADS);
-//Rumah
-coba(-8,-2,8,-2,8,2,-8,2,-5,3,0);//rumah
-coba(2,-2,8,-2,8,2,2,2,-5,4,0);//kamar
-coba(-8,-2,-7,-2,-7,2,-8,2,-5,4,0);//dindingpagar kiri
-coba(-8,-2,-7,-2,-7,0,-8,0,-5,5.5,0);
-coba(7,-2,8,-2,8,0,7,0,-5,5.5,0);
-cobaini(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
-glColor3f(0.0, 0.0, 0.0);//plafon depan
-glVertex3d(8,1.8,-3);
-glVertex3d(-8,1.8,-3);
-glVertex3d(-8,1.8,4.5);
-glVertex3d(8,1.8,4.5);//
-glVertex3d(8,1.8,4.5);
-glVertex3d(-8,1.8,4.5);
-glVertex3d(-8,2,4.5);
-glVertex3d(8,2,4.5);
-glColor3f(0.0, 0.0, 0.0);//plafon belakang
-glVertex3d(8,1.8,-5.5);
-glVertex3d(-8,1.8,-5.5);
-glVertex3d(-8,1.8,4.5);
-glVertex3d(8,1.8,4.5);
-glVertex3d(8,1.8,-5.5);
-glVertex3d(-8,1.8,-5.5);
-glVertex3d(-8,2,-5.5);
-glVertex3d(8,2,-5.5);
-glVertex3d(4,-0.5,4.1);//jendela kamar
-glVertex3d(6,-0.5,4.1);
-glVertex3d(6,1,4.1);
-glVertex3d(4,1,4.1);
-glVertex3d(2.7,-0.5,4.1);//jendela kamar2
-glVertex3d(3.8,-0.5,4.1);
-glVertex3d(3.8,1,4.1);
-glVertex3d(2.7,1,4.1);
-glVertex3d(-2,-2,3.05);//pintu depan
-glVertex3d(2,-2,3.05);
-glVertex3d(2,1.3,3.05);
-glVertex3d(-2,1.3,3.05);
-glVertex3d(-2,-2,-5.05);//pintu belakang
-glVertex3d(2,-2,-5.05);
-glVertex3d(2,1,-5.05);
-glVertex3d(-2,1,-5.05);
-glVertex3d(-4.5,-1,3.05);//jendela rmh1 depan
-glVertex3d(-2.5,-1,3.05);
-glVertex3d(-2.5,1.3,3.05);
-glVertex3d(-4.5,1.3,3.05);
-glVertex3d(-6.5,-1,-5.05);//jendela rmh1 belakang
-glVertex3d(-2.5,-1,-5.05);
-glVertex3d(-2.5,1,-5.05);
-glVertex3d(-6.5,1,-5.05);
-glVertex3d(-7,-1,3.05);//jendela rmh2 depan
-glVertex3d(-4.8,-1,3.05);
-glVertex3d(-4.8,1.3,3.05);
-glVertex3d(-7,1.3,3.05);
-glVertex3d(6.5,-1,-5.05);//jendela rmh2 belakang
-glVertex3d(2.5,-1,-5.05);
-glVertex3d(2.5,1,-5.05);
-glVertex3d(6.5,1,-5.05);
-glEnd();
-glBegin(GL_TRIANGLES);
-cobain(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
-glEnd();
-glBegin(GL_LINES);
-glColor3f(0,0,0);
-glVertex3d(8,1.8,4.5);
-glVertex3d(-8,1.8,4.5);
-glVertex3d(-8,1.8,-5.5);
-glVertex3d(8,1.8,-5.5);
-glColor3f(1,0,0);
-glVertex3d(-4.5,-1,-5.05);
-glVertex3d(-4.5,1,-5.05);
-glVertex3d(4.5,-1,-5.05);
-glVertex3d(4.5,1,-5.05);
-glEnd();
-glutSwapBuffers();
-}
-void keyboard(unsigned char key, int x, int y)
-{
-switch (key) {
+unsigned int LoadTextureFromBmpFile(char *filename);
 
-case 'x':
-xpoz=xpoz+5;
-if (xpoz>360) xpoz=0;
-glutPostRedisplay();
-break;
-case 'y':
-ypoz=ypoz+5;
-if (ypoz>360) ypoz=0;
-glutPostRedisplay();
-break;
-case 'z':
-zpoz = zpoz+1;
-if (zpoz>360) zpoz=0;
-glutPostRedisplay();
-break;
-case 'w':
-b = b + 1;
-glutPostRedisplay();
-break;
-case 's':
-b = b - 1;
-glutPostRedisplay();
-break;
-case 'a':
-a = a + 1;
-glutPostRedisplay();
-break;
-case 'd':
-a = a - 1;
-glutPostRedisplay();
-break;
-case 'q':
-c = c + 1;
-glutPostRedisplay();
-break;
-case 'e':
-c = c - 1;
-glutPostRedisplay();
-break;
+void display(void) {
+	glClearStencil(0); //clear the stencil buffer
+	glClearDepth(1.0f);
+	glClearColor(0.0, 0.6, 0.8, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); //clear the buffers
+	glLoadIdentity();
+	gluLookAt(viewx, viewy, viewz, 0.0, 0.0, 5.0, 0.0, 1.0, 0.0);
+
+	glPushMatrix();
+
+	//glBindTexture(GL_TEXTURE_3D, texture[0]);
+	drawSceneTanah(_terrain, 0.3f, 0.9f, 0.0f);
+	glPopMatrix();
+
+	glPushMatrix();
+
+	//glBindTexture(GL_TEXTURE_3D, texture[0]);
+	drawSceneTanah(_terrainTanah, 0.7f, 0.2f, 0.1f);
+	glPopMatrix();
+
+	glPushMatrix();
+
+	//glBindTexture(GL_TEXTURE_3D, texture[0]);
+	drawSceneTanah(_terrainAir, 0.0f, 0.2f, 0.5f);
+	glPopMatrix();
+
+	glutSwapBuffers();
+	glFlush();
+	rot++;
+	angle++;
+
+//jalan
+    for (int i = -3; i < 4; i++)
+    {
+        glPushMatrix();
+        glTranslatef(i*22, 0.0, 25);
+        jalan();
+        glPopMatrix();
+    }
+
+    coba(-8,-2,8,-2,8,2,-8,2,-5,3,0);//rumah
+    coba(2,-2,8,-2,8,2,2,2,-5,4,0);//kamar
+    coba(-8,-2,-7,-2,-7,2,-8,2,-5,4,0);//dindingpagar kiri
+    coba(-8,-2,-7,-2,-7,0,-8,0,-5,5.5,0);
+    coba(7,-2,8,-2,8,0,7,0,-5,5.5,0);
+    cobaini(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
+    glColor3f(0.0, 0.0, 0.0);//plafon depan
+    glVertex3d(8,1.8,-3);
+    glVertex3d(-8,1.8,-3);
+    glVertex3d(-8,1.8,4.5);
+    glVertex3d(8,1.8,4.5);//
+    glVertex3d(8,1.8,4.5);
+    glVertex3d(-8,1.8,4.5);
+    glVertex3d(-8,2,4.5);
+    glVertex3d(8,2,4.5);
+    glColor3f(0.0, 0.0, 0.0);//plafon belakang
+    glVertex3d(8,1.8,-5.5);
+    glVertex3d(-8,1.8,-5.5);
+    glVertex3d(-8,1.8,4.5);
+    glVertex3d(8,1.8,4.5);
+    glVertex3d(8,1.8,-5.5);
+    glVertex3d(-8,1.8,-5.5);
+    glVertex3d(-8,2,-5.5);
+    glVertex3d(8,2,-5.5);
+    glVertex3d(4,-0.5,4.1);//jendela kamar
+    glVertex3d(6,-0.5,4.1);
+    glVertex3d(6,1,4.1);
+    glVertex3d(4,1,4.1);
+    glVertex3d(2.7,-0.5,4.1);//jendela kamar2
+    glVertex3d(3.8,-0.5,4.1);
+    glVertex3d(3.8,1,4.1);
+    glVertex3d(2.7,1,4.1);
+    glVertex3d(-2,-2,3.05);//pintu depan
+    glVertex3d(2,-2,3.05);
+    glVertex3d(2,1.3,3.05);
+    glVertex3d(-2,1.3,3.05);
+    glVertex3d(-2,-2,-5.05);//pintu belakang
+    glVertex3d(2,-2,-5.05);
+    glVertex3d(2,1,-5.05);
+    glVertex3d(-2,1,-5.05);
+    glVertex3d(-4.5,-1,3.05);//jendela rmh1 depan
+    glVertex3d(-2.5,-1,3.05);
+    glVertex3d(-2.5,1.3,3.05);
+    glVertex3d(-4.5,1.3,3.05);
+    glVertex3d(-6.5,-1,-5.05);//jendela rmh1 belakang
+    glVertex3d(-2.5,-1,-5.05);
+    glVertex3d(-2.5,1,-5.05);
+    glVertex3d(-6.5,1,-5.05);
+    glVertex3d(-7,-1,3.05);//jendela rmh2 depan
+    glVertex3d(-4.8,-1,3.05);
+    glVertex3d(-4.8,1.3,3.05);
+    glVertex3d(-7,1.3,3.05);
+    glVertex3d(6.5,-1,-5.05);//jendela rmh2 belakang
+    glVertex3d(2.5,-1,-5.05);
+    glVertex3d(2.5,1,-5.05);
+    glVertex3d(6.5,1,-5.05);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+    cobain(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
+    glEnd();
+    glBegin(GL_LINES);
+    glColor3f(0,0,0);
+    glVertex3d(8,1.8,4.5);
+    glVertex3d(-8,1.8,4.5);
+    glVertex3d(-8,1.8,-5.5);
+    glVertex3d(8,1.8,-5.5);
+    glColor3f(1,0,0);
+    glVertex3d(-4.5,-1,-5.05);
+    glVertex3d(-4.5,1,-5.05);
+    glVertex3d(4.5,-1,-5.05);
+    glVertex3d(4.5,1,-5.05);
+    glEnd();
+    glutSwapBuffers(); /*Fungsi berikutnya adalah glutSwapBuffers(),
+    yaitu digunakan untuk menukar bagian belakan buffer menjadi buffer
+    layar (screen buffer). Dalam modus double-buffered, perintah
+    menggambar pada layar, tetapi menggambar pada buffer belakang layar.
+    Pada saat ingin menampilkan gambar, gambar yang berada di buffer
+    belakang layar dipindahkan ke buffer layar, sehingga menghasilkan
+    animasi yang sempurna.*/
 }
+
+
+
+void init(void) {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_CULL_FACE);
+
+	_terrain = loadTerrain("heightmap.bmp", 20);
+	_terrainTanah = loadTerrain("heightmapTanah.bmp", 20);
+	_terrainAir = loadTerrain("heightmapAir.bmp", 20);
+
+	//binding texture
+
 }
-int main(int argc, char** argv)
-{
-glutInit(&argc, argv);
-glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-glutInitWindowSize (750, 600);
-glutInitWindowPosition (50, 50);
-glutCreateWindow("Rumah 3D|09_109,09_123,09_128");
-init ();
-glutDisplayFunc(display);
-glutKeyboardFunc(keyboard);
-glutMainLoop();
-return 0;
+
+static void kibor(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_HOME:
+		viewy++;
+		break;
+	case GLUT_KEY_END:
+		viewy--;
+		break;
+	case GLUT_KEY_UP:
+		viewz--;
+		break;
+	case GLUT_KEY_DOWN:
+		viewz++;
+		break;
+
+	case GLUT_KEY_RIGHT:
+		viewx++;
+		break;
+	case GLUT_KEY_LEFT:
+		viewx--;
+		break;
+
+	case GLUT_KEY_F1: {
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+		glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	}
+		;
+		break;
+	case GLUT_KEY_F2: {
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient2);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse2);
+		glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	}
+		;
+		break;
+	default:
+		break;
+	}
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	if (key == 'd') {
+
+		spin = spin - 1;
+		if (spin > 360.0)
+			spin = spin - 360.0;
+	}
+	if (key == 'a') {
+		spin = spin + 1;
+		if (spin > 360.0)
+			spin = spin - 360.0;
+	}
+	if (key == 'q') {
+		viewz++;
+	}
+	if (key == 'e') {
+		viewz--;
+	}
+	if (key == 's') {
+		viewy--;
+	}
+	if (key == 'w') {
+		viewy++;
+	}
+}
+
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, (GLfloat) w / (GLfloat) h, 0.1, 1000.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_STENCIL | GLUT_DEPTH); //add a stencil buffer to the window
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tugas Besar Grafkom-Rumah 3D");
+	init();
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutReshapeFunc(reshape);
+	glutSpecialFunc(kibor);
+
+	glutKeyboardFunc(keyboard);
+
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+	glColorMaterial(GL_FRONT, GL_DIFFUSE);
+
+//    glutTimerFunc(25, putar, 0);
+	glutMainLoop();
+	return 0;
 }
